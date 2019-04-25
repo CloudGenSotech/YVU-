6810574c9a8e9ffc1a043f5656e7b11b012aec05
Ditvi Techonologies <sriurr@gmail.com>
2019-04-25 15:14:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,11 +2,11 @@ class test
 {
 public static void main(String[] args)
 {
-int a,b,c;
+int a,b,c,
 a=40;
 b=20;
 c=100;
 c=a+b+c;
-System.out.println("The sum of three values "+c);
+System.out.println("The sum of three values ",c);
 }
-}
+
